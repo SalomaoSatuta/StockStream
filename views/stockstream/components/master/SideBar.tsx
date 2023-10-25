094d76114273ea097953c66dd676989ec2713e32
Salomão <salomaohequele95@gmail.com>
2023-10-25 16:13:45 +0100

    Font selected

---
+++ views/stockstream/components/master/SideBar.tsx
@@ -0,0 +1,22 @@
+import '../../styles/sidebar.modules.css';
+
+export default function Sidebar(){
+    return (
+        <>
+            <div className="sidebar pt-4">
+                <div className='upside'>
+                    <div className='text-center'>
+                        <img src='https://placehold.co/100x100' className='img-thumbnail' />
+                    </div>
+
+                    <div>
+                    asdasdasd
+                    </div>
+                </div>
+                <div className='downside'>
+                    asdasdasd
+                </div>
+            </div>
+        </>
+    )
+}
--- views/stockstream/pages/patrimonio/home.tsx
+++ views/stockstream/pages/patrimonio/home.tsx
@@ -1,3 +1,31 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
+import Navbar from "../../components/master/Navbar";
+import SideBar from "@/components/master/SideBar";
+
 export default function (){
-    return (<><h1>Olá</h1></>)
+    return (
+            <>
+                <Head>
+                    <meta name="keywords" content="HTML, CSS, JavaScript"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300&display=swap" rel="stylesheet"/>
+
+                    <title>Home - Património</title>
+                </Head>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-2 px-0">
+                            <SideBar />
+                        </div>
+                        <div className="col-md-10">
+                            <Navbar />
+                        </div>
+                    </div>
+                </div>
+
+            </>
+        )
 }
