924602f9fed962c4b7c86d2a32249704db33fbf5
Salomão <salomaohequele95@gmail.com>
2023-10-26 18:05:19 +0100

    let us start to have fun now

--- views/stockstream/components/master/SideBar.tsx
+++ views/stockstream/components/master/SideBar.tsx
@@ -4,18 +4,38 @@ export default function Sidebar(){
     return (
         <>
             <div className="sidebar pt-4">
+
+                <div className='text-center'>
+                    <img src='https://placehold.co/100x100' className='img-thumbnail' />
+                </div>
+
                 <div className='upside'>
-                    <div className='text-center'>
-                        <img src='https://placehold.co/100x100' className='img-thumbnail' />
+
+                    <div className='row mt-3 menuListDiv'>
+                        <div className='col-md-10 m-auto'>
+                            
+                            <ul className='myList'>
+                                <li>Resumo</li>
+                                <li className='mt-2'>Dashboard</li>
+                                <li>Perfil</li>
+                                <li>Estatísticas</li>
+                            </ul>
+
+                            <ul className='myList'>
+                                <li className='mt-3'>Gestão de Stock</li>
+                                <li className='mt-2'>Funcionários</li>
+                                <li>Solicitações</li>
+                                <li>Fornecimentos</li>
+                                <li>Produtos</li>
+                                <li>Fornecedores</li>
+                            </ul>
+
+
+                        </div>
                     </div>
 
-                    <div>
-                    asdasdasd
-                    </div>
                 </div>
-                <div className='downside'>
-                    asdasdasd
-                </div>
+
             </div>
         </>
     )
